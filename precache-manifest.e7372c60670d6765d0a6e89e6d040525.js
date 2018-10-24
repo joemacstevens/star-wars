96764c8354b8db30a5fed26f7620bdd4846b8f20
Joe Stevens <joemac79@gmail.com>
2018-10-23 21:58:01 -0400

    Updates

---
+++ precache-manifest.e7372c60670d6765d0a6e89e6d040525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48e867e8c4414f7eb271",
+    "url": "/star-wars/static/js/runtime~main.48e867e8.js"
+  },
+  {
+    "revision": "185affe7e13b70a2d10d",
+    "url": "/star-wars/static/js/main.185affe7.chunk.js"
+  },
+  {
+    "revision": "2776c28eb41c9585523e",
+    "url": "/star-wars/static/js/1.2776c28e.chunk.js"
+  },
+  {
+    "revision": "185affe7e13b70a2d10d",
+    "url": "/star-wars/static/css/main.0ef3eac0.chunk.css"
+  },
+  {
+    "revision": "22285e2841fffbe0542586ecd2e08168",
+    "url": "/star-wars/index.html"
+  }
+];
